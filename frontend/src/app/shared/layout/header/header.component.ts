059785d9dfebfddd2a68e7c7ddca05b74686e988
Olga Boris <olya_boris97@mail.ru>
2023-07-09 22:07:14 +0300

    update header

--- frontend/src/app/shared/layout/header/header.component.ts
+++ frontend/src/app/shared/layout/header/header.component.ts
@@ -1,10 +1,22 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {CategoryService} from "../../services/category.service";
+import {CategoryType} from "../../../../types/category.type";
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.scss']
 })
-export class HeaderComponent {
+export class HeaderComponent implements OnInit {
 
+  categories: CategoryType[] = [];
+  constructor(private categoryService: CategoryService) {
+  }
+
+  ngOnInit(): void {
+    this.categoryService.getCategories()
+      .subscribe((categories: CategoryType[]) => {
+        this.categories = categories;
+      })
+  }
 }
