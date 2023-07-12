ce06461a725aea9bcc384d3258c2f6cb2f1590b8
Olga Boris <olya_boris97@mail.ru>
2023-07-12 17:47:33 +0300

    added form validation

--- frontend/src/app/views/user/login/login.component.ts
+++ frontend/src/app/views/user/login/login.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
+import {FormBuilder, Validators} from "@angular/forms";
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
-export class LoginComponent {
+export class LoginComponent implements OnInit {
 
+  loginForm = this.fb.group({
+    email: ['', [Validators.email, Validators.required]],
+    password: ['', [Validators.required]],
+    rememberMe: [false]
+  })
+
+  constructor(private fb: FormBuilder) {
+  }
+
+  ngOnInit(): void {
+  }
+
+  login() {
+
+  }
 }
