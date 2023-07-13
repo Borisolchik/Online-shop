1a1b8f38d22b24eaa631b8f58668c2840ff28f73
Olga Boris <olya_boris97@mail.ru>
2023-07-13 16:14:56 +0300

    update header-component

--- frontend/src/app/views/user/login/login.component.ts
+++ frontend/src/app/views/user/login/login.component.ts
@@ -47,7 +47,8 @@ export class LoginComponent implements OnInit {
               throw new Error(error);
             }
 
-            //set tokens
+            this.authService.setTokens(loginResponse.accessToken, loginResponse.refreshToken);
+            this.authService.userId = loginResponse.userId;
             this._snackBar.open('Вы успешно авторизовались');
             this.router.navigate(['/']);
           },
