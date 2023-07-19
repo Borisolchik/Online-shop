7adb94ffb1ff803b65cb8655eafc1517c2f344c7
Olga Boris <olya_boris97@mail.ru>
2023-07-19 15:02:12 +0300

    added product-card-component

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PasswordRepeatDirective} from "./directives/password-repeat.directive";
+import { ProductCardComponent } from './components/product-card/product-card.component';
 
 
 
 @NgModule({
-  declarations: [PasswordRepeatDirective],
+  declarations: [PasswordRepeatDirective, ProductCardComponent],
   imports: [
     CommonModule
   ],
