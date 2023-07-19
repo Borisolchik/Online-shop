98b3b7344840171987f7cb1152e2627e7161e64f
Olga Boris <olya_boris97@mail.ru>
2023-07-19 16:38:12 +0300

    added styles for product-card

--- frontend/src/app/shared/shared.module.ts
+++ frontend/src/app/shared/shared.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {PasswordRepeatDirective} from "./directives/password-repeat.directive";
 import { ProductCardComponent } from './components/product-card/product-card.component';
+import {RouterModule} from "@angular/router";
 
 
 
 @NgModule({
   declarations: [PasswordRepeatDirective, ProductCardComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule
   ],
   exports: [PasswordRepeatDirective]
 })
