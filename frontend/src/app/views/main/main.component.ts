8f66bd90c1a9135f9bc22514d74b331d6e6653ed
Olga Boris <olya_boris97@mail.ru>
2023-07-20 18:17:17 +0300

    added product-service

--- frontend/src/app/views/main/main.component.ts
+++ frontend/src/app/views/main/main.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import {ProductService} from "../../shared/services/product.service";
+import {ProductType} from "../../../types/product.type";
 
 @Component({
   selector: 'app-main',
   templateUrl: './main.component.html',
   styleUrls: ['./main.component.scss']
 })
-export class MainComponent {
+export class MainComponent implements OnInit {
 
+  products: ProductType[] = [];
+
+  constructor(private productService: ProductService) {
+  }
+
+  ngOnInit(): void {
+    this.productService.getBestProducts()
+      .subscribe((data: ProductType[]) => {
+        this.products = data;
+      })
+  }
 }
+
